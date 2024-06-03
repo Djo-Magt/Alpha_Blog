a35daf738b434665c795233f97ca5a707c5fb1e9
Magnet Jonathan <j.magnet@outlook.com>
2024-06-03 10:54:45 +0200

    ajout article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,36 @@
+class ArticlesController < ApplicationController
+
+  before_action  :set_article, only: [:show]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    if @residence.save
+
+    else
+
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+end
